a487378a5dd36e2b9bc9328091de9f1f37843703
gulumjan <zakirovagulumjan53@gmail.com>
2024-11-21 18:49:20 +0600

    fix

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -5,7 +5,7 @@ import {
 } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: ``,
+  baseUrl: `${process.env.NEXT_PUBLIC_API_URL}`,
 });
 
 const baseQueryExtended: BaseQueryFn = (args, api, extraOptions) =>
